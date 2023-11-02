86d676fa865a899544ff5b7043ed42db64231624
KrGDarkSide <jul.mdama08@gmail.com>
2023-11-02 12:32:41 +0100

    Added support for digits in the Caesa cipher.

--- caesar.cpp
+++ caesar.cpp
@@ -2,8 +2,7 @@
 #include "ui_caesar.h"
 #include <QMessageBox>
 
-int alphabet_big[] = { 0x41, 0x104, 0x42, 0x43, 0x106, 0x44, 0x45, 0x118, 0x46, 0x47, 0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x141, 0x4d, 0x4e, 0x143, 0x4f, 0xd3, 0x50, 0x51, 0x52, 0x53, 0x15a, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 0x179, 0x17b };
-int alphabet_small[] = { 0x61, 0x105, 0x62, 0x63, 0x107, 0x64, 0x65, 0x119, 0x66, 0x67, 0x68, 0x69, 0x6a, 0x6b, 0x6c, 0x142, 0x6d, 0x6e, 0x144, 0x6f, 0xf3, 0x70, 0x71, 0x72, 0x73, 0x15b, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7a, 0x17a, 0x17c };
+int alphabet[] = { 0x61, 0x105, 0x62, 0x63, 0x107, 0x64, 0x65, 0x119, 0x66, 0x67, 0x68, 0x69, 0x6a, 0x6b, 0x6c, 0x142, 0x6d, 0x6e, 0x144, 0x6f, 0xf3, 0x70, 0x71, 0x72, 0x73, 0x15b, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7a, 0x17a, 0x17c };
 
 
 Caesar::Caesar(QWidget *parent) :
@@ -36,28 +35,24 @@ QString caesar_cipher_encryption(QString text, int shift)
         {
             QChar current_char = text[i];
 
-            for (int j = 0; j < 35; j++)
+            if (current_char >= QChar(0x30) && current_char <= QChar(0x39))
             {
-                if (current_char == QChar(alphabet_big[j]))
+                new_word += current_char;
+            }
+            else
+            {
+                for (int j = 0; j < 35; j++)
                 {
-                    if (j + shift < 35)
+                    if (current_char == QChar(alphabet[j]))
                     {
-                        new_word += QChar(alphabet_big[j + shift]);
-                    }
-                    else
-                    {
-                        new_word += QChar(alphabet_big[j + shift - 35]);
-                    }
-                }
-                else if (current_char == QChar(alphabet_small[j]))
-                {
-                    if (j + shift < 35)
-                    {
-                        new_word += QChar(alphabet_small[j + shift]);
-                    }
-                    else
-                    {
-                        new_word += QChar(alphabet_small[j + shift - 35]);
+                        if (j + shift < 35)
+                        {
+                            new_word += QChar(alphabet[j + shift]);
+                        }
+                        else
+                        {
+                            new_word += QChar(alphabet[j + shift - 35]);
+                        }
                     }
                 }
             }
@@ -82,28 +77,24 @@ QString caesar_cipher_decryption(QString text, int shift)
         {
             QChar current_char = text[i];
 
-            for (int j = 0; j < 35; j++)
+            if (current_char >= QChar(0x30) && current_char <= QChar(0x39))
             {
-                if (current_char == QChar(alphabet_big[j]))
+                new_word += current_char;
+            }
+            else
+            {
+                for (int j = 0; j < 35; j++)
                 {
-                    if (j - shift >= 0)
+                    if (current_char == QChar(alphabet[j]))
                     {
-                        new_word += QChar(alphabet_big[j - shift]);
-                    }
-                    else
-                    {
-                        new_word += QChar(alphabet_big[j - shift + 35]);
-                    }
-                }
-                else if (current_char == QChar(alphabet_small[j]))
-                {
-                    if (j - shift >= 0)
-                    {
-                        new_word += QChar(alphabet_small[j - shift]);
-                    }
-                    else
-                    {
-                        new_word += QChar(alphabet_small[j - shift + 35]);
+                        if (j - shift >= 0)
+                        {
+                            new_word += QChar(alphabet[j - shift]);
+                        }
+                        else
+                        {
+                            new_word += QChar(alphabet[j - shift + 35]);
+                        }
                     }
                 }
             }
@@ -121,7 +112,7 @@ void Caesar::on_Button_encrypt_clicked()
 {
     ui->textEdit2->clear();
 
-    QString text = ui->textEdit1->toPlainText();
+    QString text = (ui->textEdit1->toPlainText()).toLower();
     int shift = ui->lineEdit1->text().toInt();
 
     if(text.isEmpty() || shift < 0)
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,9 +8,12 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    // shows window
     ui->setupUi(this);
+    // Add image to button, then size
     ui->Button_info->setIcon(QIcon(":/new/prefix1/Resource/info.png"));
     ui->Button_info->setIconSize(QSize(23, 23));
+    // setToolTip shows tip when mous is on button
     ui->Button_info->setToolTip("<strong>encryptor</strong>"
                                 "<table>"
                                 "<tr><td>Version:</td>"
@@ -27,25 +30,27 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
+// Button linking to the Caesar cipher
 void MainWindow::on_Button_Caesar_clicked()
 {
+    // hide main window
     hide();
     Caesar caesarWindow;
     caesarWindow.setModal(true);
     caesarWindow.exec();
 }
 
-
+// Button linking to the Polybius square
 void MainWindow::on_Button_Polybius_clicked()
 {
+    // hide main window
     hide();
     Polybius polybiusWindow;
     polybiusWindow.setModal(true);
     polybiusWindow.exec();
 }
 
-
+// Open dialogbox with informations {version, autho ...}
 void MainWindow::on_Button_info_clicked()
 {
     QMessageBox::information(this, "Informations", "<strong>encryptor</strong><table><tr><td>Version:</td><td>0.0.1</td></tr><tr><td>Creator:</td><td>Krystian Grzesiak<td></tr><tr><td>Contact:</td><td>jul.mdama08@gmail.com</td></tr></table>", QMessageBox::Ok);
