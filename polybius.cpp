8714f87449fd720608e043c29558117e8f0aeacf
KrGDarkSide <jul.mdama08@gmail.com>
2023-11-28 16:24:47 +0100

    A little changes.

--- polybius.cpp
+++ polybius.cpp
@@ -7,7 +7,6 @@
 
 const int alphabet[] = { 0x61, 0x105, 0x62, 0x63, 0x107, 0x64, 0x65, 0x119, 0x66, 0x67, 0x68, 0x69, 0x6a, 0x6b, 0x6c, 0x142, 0x6d, 0x6e, 0x144, 0x6f, 0xf3, 0x70, 0x71, 0x72, 0x73, 0x15b, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7a, 0x17a, 0x17c };
 QChar square[5][7];
-int additional_difficulties = 0;
 
 void generate_new_array(QChar arr[5][7])
 {
@@ -44,6 +43,65 @@ void generate_new_array(QChar arr[5][7])
     }
 }
 
+
+// CONVERT QString to octal number saved in QString type.
+QString toOCT(QString message)
+{
+    QString new_message = "#";
+    // RAND EVEN NUMBER (It will symbolize that our messzge is encrypted using octal.)
+    new_message += QString::number((rand() % 5) * 2);
+
+    // Run across all characters
+    for (int i = 1; i < message.length(); i += 2)
+    {
+        // Check if element is number
+        if (message[i - 1] == ':')
+        {
+            new_message = new_message + message[i - 1] + message[i] + 0;
+        }
+        else
+        {
+            // create number from two other numbers. Two because one letter in normal encryption give us two number (column; row)
+            int number = message[i - 1].digitValue() * 10 + message[i].digitValue();
+            QString oct = "";
+
+            while (number != 0)
+            {
+                oct = QString::number(number % 8) + oct;
+                number /= 8;
+            }
+
+            // ADDing '0' to QString because our table is 7 / 5 and 75 is 113.
+            //      So we need string containing 3 characters.
+            if (oct.length() != 3)
+            {
+                oct = "0" + oct;
+            }
+
+            new_message += oct;
+            oct = "";
+        }
+    }
+
+    return new_message;
+}
+
+// CONVERT from octal to encrypted message.
+QString fromOCT(QString message)
+{
+    QString new_message = "";
+
+    for (int i = 0; i < message.length(); i += 3)
+    {
+        // Checking if element is numberr
+        if (message[i] == ":") { new_message += message[i++]; }
+        else
+        {
+            // ZAMIANA Z oct na dziesiątkowy.
+        }
+    }
+}
+
 Polybius::Polybius(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Polybius)
@@ -74,6 +132,7 @@ Polybius::~Polybius()
     delete ui;
 }
 
+// ENCRYPTION Button
 QString polibius_square_encryption(QString text, QChar arr[5][7])
 {
     QString new_word = "";
@@ -109,69 +168,39 @@ QString polibius_square_encryption(QString text, QChar arr[5][7])
     return new_word;
 }
 
+// DECRYPTION Button
 QString polibius_square_decryption(QString text, QChar arr[5][7])
 {
     QString new_word = "";
 
+    // Checking if our code is encrypted in additional way.
     if (text[0] == '#')
     {
-        if (text[1] % 2 == 0)
+        if (text[1].digitValue() % 2 == 0 || text[1].digitValue() == 0)
         {
-            for (int i = 2; i <text.length(); i += 2)
-            {
-                int oct_num = text[i].digitValue() * 10 + text[i + 1].digitValue();
-            }
+            // zapis do text
         }
         else
         {
 
         }
     }
-    else
+    for (int i = 0; i < text.length(); i += 2)
     {
-        for (int i = 0; i < text.length(); i += 2)
-        {
-            QChar current_char1 = text[i];
-            QChar current_char2 = text[i + 1];
+        QChar current_char1 = text[i];
+        QChar current_char2 = text[i + 1];
 
-            if(current_char1 == ':')
-            {
-                new_word += current_char2;
-            }
-            else
-            {
-                new_word += arr[current_char2.digitValue() - 1][current_char1.digitValue() - 1];
-            }
+        if(current_char1 == ':')
+        {
+            new_word += current_char2;
+        }
+        else
+        {
+            new_word += arr[current_char2.digitValue() - 1][current_char1.digitValue() - 1];
         }
     }
 
     return new_word;
-}
-
-// CONVERT QString to octal number saved in QString type.
-QString toOCT(QString message)
-{
-    QString new_message = "#";
-    // RAND EVEN NUMBER (It will symbolize that our messzge is encrypted using octal.)
-    new_message += QString::number((rand() % 5) * 2);
-
-    // Run across all characters
-    for (int i = 1; i < message.length(); i += 2)
-    {
-        // create number from two other numbers. Two because one letter in normal encryption give us two number (column; row)
-        int number = message[i - 1].digitValue() * 10 + message[i].digitValue();
-        QString oct = "";
-
-        while (number != 0)
-        {
-            oct = QString::number(number % 8) + oct;
-            number /= 8;
-        }
-        new_message += oct;
-        oct = "";
-    }
-
-    return new_message;
 }
 
 void Polybius::on_pushButton2_clicked()
