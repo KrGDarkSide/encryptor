3b7b90db02fc95229a39efdd41b36ff78a0cc4d4
KrGDarkSide <jul.mdama08@gmail.com>
2024-02-08 18:53:51 +0100

    Homophonic - encryption (Not working)

--- homophonic.cpp
+++ homophonic.cpp
@@ -4,6 +4,11 @@
 #include <QMessageBox>
 #include <QFileDialog>
 #include <QDataStream>
+#include <QMultiMap>
+#include <cstdlib>
+#include <ctime>
+
+const int alphabet[] = { 0x61, 0x105, 0x62, 0x63, 0x107, 0x64, 0x65, 0x119, 0x66, 0x67, 0x68, 0x69, 0x6a, 0x6b, 0x6c, 0x142, 0x6d, 0x6e, 0x144, 0x6f, 0xf3, 0x70, 0x71, 0x72, 0x73, 0x15b, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7a, 0x17a, 0x17c };
 
 Homophonic::Homophonic(QWidget *parent) :
     QDialog(parent),
@@ -20,10 +25,11 @@ Homophonic::Homophonic(QWidget *parent) :
     ui->En_inputKey->setValidator(validator);
     ui->De_inputKey->setValidator(validator);
 
-    // Checking validation of entered text
     // Result read only
     ui->En_outputText->setReadOnly(true);
     ui->De_outputText->setReadOnly(true);
+
+    srand (time(NULL));
 }
 
 Homophonic::~Homophonic()
@@ -31,12 +37,94 @@ Homophonic::~Homophonic()
     delete ui;
 }
 
+
+
+// Create multiMap that as keys have alphabet characters and as values numbers from 0 to 999.
+QMultiMap<QChar, int> create_alphabetMap(QString &key)
+{
+    QMultiMap<QChar, int> map;
+    int num = 0;
+
+    // Fill multiMap with numbers depending on the key word
+    for (int i = 0; i < key.length(); i++)
+    {
+        bool find_kLetter = false;
+        for (int j = 0; j < 35; j++)
+        {
+            if (key[i] == QChar(alphabet[j]))
+            {
+                map.insert(QChar(alphabet[j]), num);
+                num++;
+                find_kLetter = true;
+            }
+            if (find_kLetter)
+            {
+                if (j == 34)
+                {
+                    j = -1;
+                }
+                map.insert(QChar(alphabet[j]), num);
+                num++;
+            }
+        }
+    }
+
+    // After used key word start filling multimap form 'a' to 'ż' by numbers till num will be 999.
+    while (num != 999)
+    {
+        for (int i = 0; i < 35; i++)
+        {
+            map.insert(QChar(alphabet[i]), num);
+            num++;
+            if (num >= 999)
+            {
+                break;
+            }
+        }
+    }
+
+    return map;
+}
+
+QString prepare_value(int val)
+{
+    QString new_value = QString::number(val);
+
+    if (new_value.length() == 1) { new_value = "00" + new_value; }
+    else if (new_value.length() == 2) { new_value = "0" + new_value; }
+
+    return new_value;
+}
+
+QString homophonic_encryption(QString text, QString key)
+{
+    QMultiMap<QChar, int> alphabetMap = create_alphabetMap(key);
+    QString new_word = "";
+
+    for (int i = 0; i < text.length(); i++)
+    {
+        QList<int> key_values;
+        for (auto x = alphabetMap.begin(); x != alphabetMap.end(); x++)
+        {
+            if (x.key() == text[i])
+            {
+                key_values.append(x.value());
+            }
+        }
+
+        // generate pseudo random number
+        int index = rand() % key_values.size();
+        new_word += prepare_value(key_values.at(index));
+    }
+
+    return new_word;
+}
+
+
+
 // ENCRYPT button
 void Homophonic::on_En_Button_clicked()
 {
-    // REGULAR EXPRESSION
-    static QRegularExpression rx("^[\\p{L}0-9\\s]*$");
-
     // Take text from QTextEdit and convert it to lowercase
     QString text = (ui->En_inputText->toPlainText()).toLower();
     QString key = (ui->En_inputKey->text()).toLower();
@@ -52,14 +140,7 @@ void Homophonic::on_En_Button_clicked()
     }
     else
     {
-        if (!text.contains(rx))
-        {
-            QMessageBox::warning(this, "Warning", "Invalid input!\nOnly letters and numbers!", QMessageBox::Ok);
-        }
-        else
-        {
-            // FUNCTION
-        }
+        ui->En_outputText->append(homophonic_encryption(text, key));
     }
 }
 
@@ -68,6 +149,8 @@ void Homophonic::on_De_Button_clicked()
 {
 
 }
+
+
 
 // IMPORT key for encryption and next method for decryption
 void Homophonic::on_En_importBt_clicked()
